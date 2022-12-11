4f22b100b09993618e5ad1ceb91349478b1e015c
Saddam Hafiz <saddam.hafiz.dhaka@gmail.com>
2022-12-12 00:42:27 +0600

    data, timmer added with conflict

---
+++ js/arrayMap.js
@@ -0,0 +1,12 @@
+// let number = [
+//     {name : "Saddam Hafiz", roll : 01, marks : 90},
+//     {name : "Rejaon", roll : 02, marks : 54},
+//     {name : "Kamrul", roll : 03, marks : 65}
+// ];
+
+// number.map((name, value, arr) => {
+//     console.log("My name is " + name.name);
+//     console.log("My roll is " + name.roll);
+//     console.log("My marks is :", name.marks);
+//     console.log(arr);
+// });
--- js/script.js
+++ js/script.js
@@ -1,71 +1,159 @@
-const cardList = [
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-  {
-    name: "saddam",
-  },
-];
+const variantList = {
+    color : [{
+      type : "blue",
+      value : []
+    },
+    {
+      type : "orange",
+      value : [] 
+    },
+    {
+      type : "violet",
+      value : []
+    },
+    {
+      type : "green",
+      value : []
+    }],
+
+    shape :[ {
+      type: "cricle",
+      value : []
+    },
+    {
+      type: "triangle",
+      value : []
+    },
+    {
+      type: "squre",
+      value: []
+    },
+    {
+      type: "star",
+      value: []
+    }],
+
+    style :[{
+      type : "dot",
+      value : []
+    },
+    {
+      type : "scale",
+      value : []
+    },
+    {
+      type : "non",
+      value : []
+    },
+    {
+      type : "fill",
+      value : []
+    }]
+}
+const colorList = ["blue", "orange", "violet", "green"];
+const imageList = [];
+let i = 0;
+
+colorList.forEach((color) => {
+   const shape = ["circle", "triangle", "squre", "star"];
+    shape.forEach((shape) => {
+      
+      const style = ["dot", "scale", "non", "fill"];
+
+      style.forEach((style) => {
+        imageList.push({img: `${color}-${shape}-${style}`});
+        i += 1;
+
+        const word = imageList[i-1];
+        const letter = word.img.charAt(0);
+
+        if (letter == "b") {
+          variantList.color[0].value.push(`${color}-${shape}-${style}`);
+          variantList.shape[0].value.push(`${color}-${shape}-${style}`);
+          variantList.style[0].value.push(`${color}-${shape}-${style}`);
+        }
+        else if(letter == "o") {
+          variantList.color[1].value.push(`${color}-${shape}-${style}`);
+          variantList.shape[1].value.push(`${color}-${shape}-${style}`);
+          variantList.style[1].value.push(`${color}-${shape}-${style}`);
+        }
+        else if(letter == "v") {
+          variantList.color[2].value.push(`${color}-${shape}-${style}`);
+          variantList.shape[2].value.push(`${color}-${shape}-${style}`);
+          variantList.style[2].value.push(`${color}-${shape}-${style}`);
+        }
+        else if(letter == "g") {
+          variantList.color[3].value.push(`${color}-${shape}-${style}`);
+          variantList.shape[3].value.push(`${color}-${shape}-${style}`);
+          variantList.style[3].value.push(`${color}-${shape}-${style}`);
+        }
+      })
+   })
+  
+})
+
+// console.log(variantList.color);
+// console.log(variantList.shape);
+// console.log(variantList.style);
+
+// console.log(imageList);
+// console.log(typeof(imageList)); 
 
 const cardListContainer = document.getElementById("card_list");
 
 // create div 
 
-const divCreator = (div_class) => {
+const divCreator = (imgLink, div_class) => {
     const imgTag = document.createElement('img');
-    imgTag.src = "https://cdn-icons-png.flaticon.com/512/686/686751.png";
+    imgTag.src = imgLink;
     imgTag.alt = "No image show";
+    imgTag.style.height = "60px";
+    imgTag.style.width = "60px";
 
     const divTage = document.createElement("div");
     divTage.className = `item item_${div_class}`;
+
     divTage.appendChild(imgTag);
     cardListContainer.appendChild(divTage);
 }
 
 // genaret div
 
-cardList.map((name, num) => {
-    divCreator(num+1);
-});
+// cardList.map((name, num) => {
+//     divCreator(name.src, num+1);
+// });
+
+// Timmer Functions
+
+class CardGame {
+    allCardlist() {
+      return imageList;
+    }
+    counDown(count = 60) {
+      let counter = count;
+      let timmer = setInterval((e) => {
+        counter--;
+        if("0" == counter) {
+          clearInterval(timmer);
+        }
+        
+        // Update counter
+        const timmerTage = document.querySelector(".timmer p");
+        timmerTage.innerHTML = `0${Math.floor(counter/60)}:${counter.toString().length == 2? counter : `0${counter}`}`; 
+      }, 1000);
+    }
+}
+
+const game = new CardGame();
+console.log(game.allCardlist());
+console.log(game.counDown(71)
+);
+
+
+
+
+
+
+
+
+
