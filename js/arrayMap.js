886f49cd99438597ab16ae31dae8d33e1c0dcce6
Mohd Rejoan <wearecrx@gmail.com>
2022-12-13 20:25:01 +0600

    get value created on arrayMap file

--- js/arrayMap.js
+++ js/arrayMap.js
@@ -1,12 +1,52 @@
-// let number = [
-//     {name : "Saddam Hafiz", roll : 01, marks : 90},
-//     {name : "Rejaon", roll : 02, marks : 54},
-//     {name : "Kamrul", roll : 03, marks : 65}
-// ];
+console.log("start array map ------------------- ");
+console.clear();
 
-// number.map((name, value, arr) => {
-//     console.log("My name is " + name.name);
-//     console.log("My roll is " + name.roll);
-//     console.log("My marks is :", name.marks);
-//     console.log(arr);
-// });
+const objectgetset = {
+    name: {
+        studentList: [
+            { studentName: "Mohd Rejoan", friends: [] },
+            { studentName: "Mahfuz Sorker", friends: [] },
+            { studentName: "Kamrul Islam", friends: [] },
+            { studentName: "Redoy", friends: [] },
+            { studentName: "Mehedi Hasan", friends: [] },
+        ],
+    },
+
+    get setFriendsname() {
+        //   this.name.studentList.map((student, index, selfList) => {
+        //       const friendName = [];
+        //       selfList.map((item, index) => {
+        //           if (item.studentName !== student.studentName) {
+        //               friendName.push(item.studentName);
+        //           }
+        //       });
+        //       student.friends = friendName;
+        //   });
+
+        // console.log(this.name.studentList)
+        console.log("hello");
+        let friendList = [];
+        for (let index = 0; index < this.name.studentList.length; index++) {
+            friendList.push(this.name.studentList[index].studentName);
+      }
+        
+      console.log(friendList);
+    },
+
+    set setFriendsname(a) {
+        // console.log("hello");
+        // let friendList = [];
+        // for (let index = 0; index < this.name.studentList.length; index++) {
+        //     friendList.push(this.name.studentList[index].studentName);
+        // }
+        // console.log(friendList);
+        // this.name.studentList[0].friends = rejoan.replace(rejoan, `${friend2}, ${friend3}, ${friend4}`);
+        // this.name.studentList[1].friends = rejoan.replace(rejoan, `${friend1}, ${friend3}, ${friend4}`);
+        // this.name.studentList[2].friends = rejoan.replace(rejoan, `${friend1}, ${friend2}, ${friend4}`);
+        // this.name.studentList[3].friends = rejoan.replace(rejoan, `${friend1}, ${friend2}, ${friend3}`);
+    },
+};
+
+// objectgetset.setFriendsname = "Mohd Rejoan";
+// console.log(objectgetset.name);
+objectgetset.setFriendsname;
--- js/script.js
+++ js/script.js
@@ -150,34 +150,6 @@ function generateRandomTableImg() {
 
 // console.log(rejoan.changegetName);
 
-const objectgetset = {
-  name: {
-    studentList: [
-      { studentName: "Mohd Rejoan", friends: [] },
-      { studentName: "Mahfuz Sorker", friends: [] },
-      { studentName: "Kamrul Islam", friends: [] },
-      { studentName: "Redoy", friends: [] },
-      { studentName: "Mehedi Hasan", friends: [] },
-    ],
-  },
-
-  set setFriendsname(rejoan){
-
-    let friendList = [];
-    for(let index = 0; index < this.name.studentList.length; index++) {
-        friendList.push(this.name.studentList[index].studentName);
-    }
-    console.log(friendList);
-
-    // this.name.studentList[0].friends = rejoan.replace(rejoan, `${friend2}, ${friend3}, ${friend4}`);
-    // this.name.studentList[1].friends = rejoan.replace(rejoan, `${friend1}, ${friend3}, ${friend4}`);
-    // this.name.studentList[2].friends = rejoan.replace(rejoan, `${friend1}, ${friend2}, ${friend4}`);
-    // this.name.studentList[3].friends = rejoan.replace(rejoan, `${friend1}, ${friend2}, ${friend3}`);
-  }
-};
-
-objectgetset.setFriendsname = "Mohd Rejoan";
-console.log(objectgetset.name);
 
 generateRandomTableImg();
 // console.log(cardImgStorage);
